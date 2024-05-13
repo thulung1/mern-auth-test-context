ff5f9a3bab1fb9ec57c77f071db4f832ffaa3a56
Shubham <shubhamrai199515@gmail.com>
2024-05-13 20:40:31 +0530

    update backend

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -52,7 +52,8 @@ const logoutUser = async (req, res) => {
 
 const profile = (req, res) => {
   const token = req.headers.authorization;
-  console.log(token)
+  console.log("Token is ", token)
+  console.log("Req headers ", req.headers)
 
   if (!token) {
     return res.status(411).json("Token not found");
